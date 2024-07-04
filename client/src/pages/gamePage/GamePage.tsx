bffd4c4db2d201acb2394549b319f17cd0ff2895
AlexanderGrebenyuk <suppose.ca@gmail.com>
2024-07-04 20:28:54 +0300

    feat/createModal

--- client/src/pages/gamePage/GamePage.tsx
+++ client/src/pages/gamePage/GamePage.tsx
@@ -1,48 +1,48 @@
 import React from 'react';
 import Theme from './Theme';
-import Quest from './Quest';
 
 export type Themes = {
-  theme: string;
+  title: string;
+  img: string;
   id: number;
 };
 
 export type Questions = {
-  quest: string;
+  name: string;
   id: number;
-  categoryId: number
+  categoryId: number;
+  answer: string;
+  img: string;
 };
 
 const themes: Themes[] = [
-  { theme: '1 theme', id: 1 },
-  { theme: '2 theme', id: 2 },
-  { theme: '3 theme', id: 3 },
+  { title: '1 theme', id: 1, img: '1 img' },
+  { title: '2 theme', id: 2, img: '2 img' },
+  { title: '3 theme', id: 3, img: '3 img' },
 ];
 
 const questions: Questions[] = [
-  { quest: '1 quest', id: 1, categoryId: 1 },
-  { quest: '2 quest', id: 2, categoryId: 3 },
-  { quest: '3 quest', id: 3, categoryId: 2 },
-  { quest: '4 quest', id: 4, categoryId: 1 },
-  { quest: '5 quest', id: 5, categoryId: 2 },
-  { quest: '6 quest', id: 6, categoryId: 1 },
-  { quest: '7 quest', id: 7, categoryId: 3 },
-  { quest: '8 quest', id: 8, categoryId: 1 },
-  { quest: '9 quest', id: 9, categoryId: 2 },
+  { name: '1 quest', id: 1, categoryId: 1, answer: 'da', img: 'https://png.pngtree.com/png-clipart/20231017/original/pngtree-burger-food-png-free-download-png-image_13329458.png' },
+  { name: '2 quest', id: 2, categoryId: 3, answer: 'da', img: 'asd' },
+  { name: '3 quest', id: 3, categoryId: 2, answer: 'da', img: 'asd' },
+  { name: '4 quest', id: 4, categoryId: 1, answer: 'da', img: 'asd' },
+  { name: '5 quest', id: 5, categoryId: 2, answer: 'da', img: 'asd' },
+  { name: '6 quest', id: 6, categoryId: 1, answer: 'da', img: 'asd' },
+  { name: '7 quest', id: 7, categoryId: 3, answer: 'da', img: 'asd' },
+  { name: '8 quest', id: 8, categoryId: 3, answer: 'da', img: 'asd' },
+  { name: '9 quest', id: 9, categoryId: 2, answer: 'da', img: 'asd' },
 ];
 
-type GamePageProps = {};
-
-const GamePage = ({}: GamePageProps): JSX.Element => {
+const GamePage: React.FC = () => {
   return (
-    <div className=" GamePage">
-      <div style={{display: 'flex', flexDirection: 'column'}}>
+    <div className="GamePage">
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
         {themes.map((theme) => (
           <Theme theme={theme} questions={questions} key={theme.id} />
         ))}
       </div>
-
     </div>
   );
 };
+
 export default GamePage;
