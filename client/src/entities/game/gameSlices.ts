dc6d7390176b694c8a2cd5c964447a97d45bee16
Vladislav Borisov <nightwayboom@gmail.com>
2024-07-05 15:09:51 +0300

    fixEndGame

--- client/src/entities/game/gameSlices.ts
+++ client/src/entities/game/gameSlices.ts
@@ -24,8 +24,13 @@ export const createNewGameLinesThunk = createAsyncThunk('create/games', () =>
   GameApi.createNewGame(),
 );
 
-export const updateAnswerQuestionThunk = createAsyncThunk('update/gameQuest', (id: number) =>
-  GameApi.answeredQuestionRight(id),
+export const updateAnswerQuestionPlusThunk = createAsyncThunk(
+  'updateRight/gameQuest',
+  (id: number) => GameApi.answeredQuestionRight(id),
+);
+export const updateAnswerQuestionMinusThunk = createAsyncThunk(
+  'updateWrong/gameQuest',
+  (id: number) => GameApi.answeredQuestionWrong(id),
 );
 
 const GameSlice = createSlice({
@@ -52,11 +57,23 @@ const GameSlice = createSlice({
         state.error = action.error.message;
         state.loading = false;
       })
-      .addCase(updateAnswerQuestionThunk.fulfilled, (state, action) => {
+      .addCase(updateAnswerQuestionPlusThunk.fulfilled, (state, action) => {
         state.gameLines = state.gameLines.map((gameLine) =>
           gameLine.id === action.payload.gameLine.id ? action.payload.gameLine : gameLine,
         );
-        state.game = action.payload.game
+        state.game = action.payload.game;
+      })
+      .addCase(updateAnswerQuestionMinusThunk.fulfilled, (state, action) => {
+        // if (action.payload.message === 'success') {
+        state.gameLines = state.gameLines.map((gameLine) =>
+          gameLine.id === action.payload.gameLine.id ? action.payload.gameLine : gameLine,
+        );
+        state.game = action.payload.game;
+        // } else if (action.payload.message === 'Game over!') {
+        //   state.gameLines = state.gameLines.map((gameLine) =>
+        //     gameLine.id === action.payload.gameLine.id ? action.payload.gameLine : gameLine,
+        //   );
+        // }
       });
   },
 });
