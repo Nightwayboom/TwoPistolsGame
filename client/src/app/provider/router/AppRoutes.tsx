a3bc126b4e7a9e5690003010c40da62b8120a1b4
AlexanderGrebenyuk <suppose.ca@gmail.com>
2024-07-05 16:14:50 +0300

    feat/addLogic

--- client/src/app/provider/router/AppRoutes.tsx
+++ client/src/app/provider/router/AppRoutes.tsx
@@ -4,6 +4,7 @@ import AuthorizationPage from '../../../pages/authPages/AuthorizationPage';
 import RegistrationPage from '../../../pages/authPages/RegistrationPage';
 import Game from '../../../pages/gamePage/GameButtonStart';
 import GamePage from '../../../pages/gamePage/GamePage';
+import Page404 from '../../../pages/404/Page404';
 
 function AppRoutes(): JSX.Element {
   return (
@@ -12,6 +13,7 @@ function AppRoutes(): JSX.Element {
       <Route path="/signIn" element={<AuthorizationPage />} />
       <Route path="/signUp" element={<RegistrationPage />} />
       <Route path="/game" element={<GamePage />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
